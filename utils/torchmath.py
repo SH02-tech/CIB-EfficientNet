b963f96f45290ed8b1e72888513ffc1ddee97493
Shao Jie Hu Chen shaojiehc@correo.ugr.es - Resp. Francisco Herrera Triguero <sjhu@ngpu>
2025-06-07 19:41:43 +0200

    src: add attribution visualization for efficientNet

--- utils/torchmath.py
+++ utils/torchmath.py
@@ -1,7 +1,12 @@
 import torch
+import copy
+
+from torchvision.models.efficientnet import MBConv
+from torchvision.ops.misc import SqueezeExcitation
 
 from zennit.composites import EpsilonPlusFlat
-from zennit.canonizers import SequentialMergeBatchNorm
+from zennit import canonizers as canonizers
+from zennit import layer as zlayer
 from crp.attribution import CondAttribution
 from crp.concepts import ChannelConcept
 from crp.helper import get_layer_names
@@ -23,32 +28,112 @@ def denormalize(tensor, mean, std):
 
     return tensor * std + mean
 
-class ZennitHandler:
-    def __init__(self, model):
-        self.model = model
-        self.model.eval()
-        self.composite = EpsilonPlusFlat([SequentialMergeBatchNorm()])
-        self.attribution = CondAttribution(model, no_param_grad=True)
-        self.layer_names = get_layer_names(model, [torch.nn.Conv2d, torch.nn.Linear])
-        self.cc = ChannelConcept()
+# Canonizer for EfficientNet obtained from:
+# Optimizing Explanations by Network Canonization and Hyperparameter Search
+# CVPR2023 Workshop
+# Pahde et al.  
 
-    def get_topk_relevances(self, input_tensor, zennit_list, layer_name, k=5):
-        """
-        Get the top-k relevances for a given layer in the model.
-        Args:
-            input_tensor (torch.Tensor): Input tensor for the model.
-            zennit_list (list): List of Zennit dictionaries containing relevant information for layer, as specified in CRP.
-            layer_name (str): Name of the layer to analyze.
-            k (int): Number of top relevances to return.
-        Returns:
-            torch.Tensor: Top-k relevances and their corresponding concept IDs.
-        """
+class SignalOnlyGate(torch.autograd.Function):
+
+    @staticmethod
+    def forward(ctx,x1,x2):
+        return x1*x2 
+
+    @staticmethod
+    def backward(ctx,grad_output):
+        return torch.zeros_like(grad_output), grad_output
+
+
+class SECanonizer(canonizers.AttributeCanonizer):
+    def __init__(self):
+        super().__init__(self._attribute_map)
+
+    @classmethod
+    def _attribute_map(cls, name, module):
+        if isinstance(module, SqueezeExcitation):
+            attributes = {
+                'forward': cls.forward.__get__(module),
+                'fn_gate': SignalOnlyGate()
+            }
+            return attributes
+        return None
+
+    @staticmethod
+    def forward(self, input):
+        scale = self._scale(input)
+        return self.fn_gate.apply(scale, input)
+
+class MBConvCanonizer(canonizers.AttributeCanonizer):
+    '''Canonizer specifically for MBConvBlock of Mobile Net v2 type models.'''
+
+    def __init__(self):
+        super().__init__(self._attribute_map)
+
+    @classmethod
+    def _attribute_map(cls, name, module):
+        if isinstance(module, MBConv):
+            attributes = {
+                'forward': cls.forward.__get__(module),
+                'canonizer_sum': zlayer.Sum()
+            }
+            return attributes
+        return None
+
+    @staticmethod
+    def forward(self, input):
+        result = self.block(input)
+        if self.use_res_connect:
+            result = self.stochastic_depth(result)
+
+            # result += input
+            result = torch.stack([input, result], dim=-1)
+            result = self.canonizer_sum(result)
+        return result
+
+class EfficientNetCanonizer(canonizers.CompositeCanonizer):
+    def __init__(self):
+        super().__init__((
+            SECanonizer(),
+            MBConvCanonizer(),
+        ))
+
+class EfficientNetBNCanonizer(canonizers.CompositeCanonizer):
+    def __init__(self):
+        super().__init__((
+            SECanonizer(),
+            MBConvCanonizer(),
+            canonizers.SequentialMergeBatchNorm()
+        ))
+
+class ZennitHandler:
+    def __init__(self, model_xmi):
+        # deep copy
+        model = copy.deepcopy(model_xmi)
+        
+        # change requires_grad to True for all parameters
+        for param in model.parameters():
+            param.requires_grad = True
+
+        model.eval()
+        self.composite = EpsilonPlusFlat([EfficientNetBNCanonizer()])
+        self.layer_names = get_layer_names(model, [torch.nn.Conv2d, torch.nn.Linear])
+        self.attribution = CondAttribution(model, no_param_grad=True)
+        self.cc = ChannelConcept()
+        
+
+    def get_heatmap_top(self, input_tensor, target_layer, label, k=5):
         input_zennit = input_tensor.clone().detach()
         input_zennit = input_zennit.unsqueeze(0)
         input_zennit.requires_grad = True
 
-        attr = self.attribution(input_zennit, zennit_list, self.composite, record_layer=self.layer_names)
-        rel_c = self.cc.attribute(attr.relevances[layer_name], abs_norm=True)
+        conditions = [{'y': [label]}]
 
-        rel_values, concept_ids = torch.topk(rel_c[0], k=k)
-        return rel_values, concept_ids, attr
+        attr = self.attribution(input_zennit, conditions, self.composite, record_layer=self.layer_names)
+        rel_c = self.cc.attribute(attr.relevances[target_layer], abs_norm=True)
+        _, concept_ids = torch.topk(rel_c[0], k)
+
+        conditions = [{target_layer: [id], 'y': [label]} for id in concept_ids]
+
+        heatmap, _, _, _ = self.attribution(input_zennit, conditions, self.composite)
+
+        return heatmap
