b005d359c8b9a63c903171ea0839f2bd496940ba
Shao Jie Hu Chen shaojiehc@correo.ugr.es - Resp. Francisco Herrera Triguero <sjhu@ngpu>
2025-06-16 19:10:33 +0200

    src: add l2 loss weight

--- model/loss.py
+++ model/loss.py
@@ -34,13 +34,10 @@ def cov_loss(features):
     mean_features = torch.mean(features, dim=0, keepdim=True)
     center_features = features - mean_features
 
-    cov = torch.matmul(center_features.T, center_features) / batch_size
-
-    id_matrix = torch.eye(cov.size(0), device = cov.device)
+    cov = torch.matmul(center_features, center_features.T) / batch_size
 
     # Compute the squared Frobenius norm of the difference
-    decorr_loss = torch.norm(cov - id_matrix, p=2) ** 2
-    decorr_loss = decorr_loss - torch.norm(torch.diag(cov - id_matrix), p=2) ** 2
+    decorr_loss = torch.norm(cov, p=2) ** 2 - torch.norm(torch.diag(cov), p=2) ** 2
 
     return decorr_loss
 
@@ -49,6 +46,20 @@ def l1_loss(features):
 
 def l2_loss(features):
     return torch.norm(features, p=2)
+
+def l2_loss_weights(weights):
+    # compute l2 loss per channel
+    if len(weights.shape) > 2:  # weights from CNN layer
+        weights = weights.view(weights.size(0), -1)
+
+    loss = 0
+    num_channels = int(weights.shape[1])
+
+    for i in range(num_channels):
+        channel_weights = weights[:, i]
+        loss += torch.norm(channel_weights, p=2)
+
+    return loss
 
 # def ortho_loss(weights):
 #     loss = 0
@@ -92,7 +103,7 @@ class XMILoss(nn.Module):
         loss_cov = cov_loss(features) if self.w_cov != 0 else torch.tensor(0.0, device=output.device)
         loss_ortho = ortho_loss(weights,  type="row") if self.w_ortho != 0 else torch.tensor(0.0, device=output.device)
         loss_l1 = l1_loss(weights) if self.w_l1 != 0 else torch.tensor(0.0, device=output.device)
-        loss_l2 = l2_loss(weights) if self.w_l2 != 0 else torch.tensor(0.0, device=output.device)
+        loss_l2 = l2_loss_weights(weights) if self.w_l2 != 0 else torch.tensor(0.0, device=output.device)
 
         loss = self.w_entropy * loss_nll + self.w_mi * loss_kl + \
             self.w_cov * loss_cov + self.w_ortho * loss_ortho + \
